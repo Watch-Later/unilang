Watch-Later/unilang

commit edc7bc0b438a87da7e0b8b8683d4858b05b938b3

Fixed missing of supporting empty symbol ('') as the name of dynamic environment by split of the vau handler class.
The empty symbol was supported since commit 35d730c9455f9eb1c36e69ab4b4cb52d741226da.
The split of vau handler also allows further native implementation of lambda abstraction creation.

Signed-off-by: He Bin <hebin@uniontech.com>

src/Forms.cpp
@@ -252,35 +252,37 @@ VauPrepareCall(Context& ctx, TermNode& term, ValueObject& parent,
 }
 
 
-class VauHandler final : private ystdex::equality_comparable<VauHandler>
+class VauHandler : private ystdex::equality_comparable<VauHandler>
 {
 private:
 	shared_ptr<TermNode> p_formals;
+	ReductionStatus(&call)(const VauHandler&, TermNode&, Context&);
 	mutable ValueObject parent;
 	mutable shared_ptr<TermNode> p_eval_struct;
-	ReductionStatus(&call)(const VauHandler&, TermNode&, Context&);
 
 public:
 	bool NoLifting = {};
 
-private:
-	string eformal{};
+protected:
+	VauHandler(shared_ptr<TermNode>&& p_fm, ValueObject&& vo, TermNode& term,
+		bool nl, ReductionStatus(&c)(const VauHandler&, TermNode&, Context&))
+		: p_formals((CheckParameterTree(Deref(p_fm)), std::move(p_fm))),
+		call(c), parent(std::move(vo)), p_eval_struct((AssertValueTags(term),
+		ShareMoveTerm(ystdex::exchange(term,
+		Unilang::AsTermNode(term.get_allocator()))))), NoLifting(nl)
+	{}
 
 public:
-	VauHandler(string&& ename, shared_ptr<TermNode>&& p_fm,
-		ValueObject&& vo, TermNode& term, bool nl)
-		: p_formals((CheckParameterTree(Deref(p_fm)), std::move(p_fm))),
-		parent(std::move(vo)), p_eval_struct((AssertValueTags(term),
-		ShareMoveTerm(ystdex::exchange(term,
-		Unilang::AsTermNode(term.get_allocator()))))), call(ename.empty()
-		? CallStatic : CallDynamic), NoLifting(nl), eformal(std::move(ename))
+	VauHandler(shared_ptr<TermNode>&& p_fm, ValueObject&& vo, TermNode& term,
+		bool nl)
+		: VauHandler(std::move(p_fm), std::move(vo), term, nl, CallStatic)
 	{}
 
 	friend bool
 	operator==(const VauHandler& x, const VauHandler& y)
 	{
 		return x.p_formals == y.p_formals && x.parent == y.parent
-			&& x.NoLifting == y.NoLifting && x.eformal == y.eformal;
+			&& x.NoLifting == y.NoLifting;
 	}
 
 	ReductionStatus
@@ -293,23 +295,6 @@ public:
 	}
 
 private:
-	void
-	BindEnvironment(Context& ctx, ValueObject&& vo) const
-	{
-		assert(!eformal.empty());
-		ctx.GetRecordRef().AddValue(eformal, std::move(vo));
-	}
-
-	static ReductionStatus
-	CallDynamic(const VauHandler& vau, TermNode& term, Context& ctx)
-	{
-		auto r_env(ctx.WeakenRecord());
-		EnvironmentGuard gd(ctx, Unilang::SwitchToFreshEnvironment(ctx));
-
-		vau.BindEnvironment(ctx, std::move(r_env));
-		return vau.DoCall(term, ctx, gd);
-	}
-
 	static ReductionStatus
 	CallStatic(const VauHandler& vau, TermNode& term, Context& ctx)
 	{
@@ -318,6 +303,7 @@ private:
 		return vau.DoCall(term, ctx, gd);
 	}
 
+public:
 	ReductionStatus
 	DoCall(TermNode& term, Context& ctx, EnvironmentGuard& gd) const
 	{
@@ -341,6 +327,42 @@ private:
 };
 
 
+class DynamicVauHandler final
+	: private VauHandler, private ystdex::equality_comparable<DynamicVauHandler>
+{
+private:
+	string eformal{};
+
+public:
+	DynamicVauHandler(string&& ename, shared_ptr<TermNode>&& p_fm,
+		ValueObject&& vo, TermNode& term, bool nl)
+		: VauHandler(std::move(p_fm), std::move(vo), term, nl, CallDynamic),
+		eformal(std::move(ename))
+	{}
+
+	YB_ATTR_nodiscard friend bool
+	operator==(const DynamicVauHandler& x, const DynamicVauHandler& y)
+	{
+		return static_cast<const VauHandler&>(x)
+			== static_cast<const VauHandler&>(y) && x.eformal == y.eformal;
+	}
+
+	using VauHandler::operator();
+
+private:
+	static ReductionStatus
+	CallDynamic(const VauHandler& vau, TermNode& term, Context& ctx)
+	{
+		auto r_env(ctx.WeakenRecord());
+		EnvironmentGuard gd(ctx, Unilang::SwitchToFreshEnvironment(ctx));
+
+		ctx.GetRecordRef().AddValue(static_cast<const DynamicVauHandler&>(
+			vau).eformal, std::move(r_env));
+		return vau.DoCall(term, ctx, gd);
+	}
+};
+
+
 template<typename _func>
 inline auto
 CheckFunctionCreation(_func f) -> decltype(f())
@@ -355,7 +377,7 @@ CheckFunctionCreation(_func f) -> decltype(f())
 	}
 }
 
-YB_ATTR_nodiscard VauHandler
+YB_ATTR_nodiscard DynamicVauHandler
 MakeVau(TermNode& term, bool no_lift, TNIter i, ValueObject&& vo)
 {
 	auto formals(ShareMoveTerm(Unilang::Deref(++i)));
@@ -363,8 +385,8 @@ MakeVau(TermNode& term, bool no_lift, TNIter i, ValueObject&& vo)
 
 	term.erase(term.begin(), ++i);
 	ClearCombiningTags(term);
-	return VauHandler(std::move(eformal), std::move(formals), std::move(vo),
-		term, no_lift);
+	return DynamicVauHandler(std::move(eformal), std::move(formals),
+		std::move(vo), term, no_lift);
 }
 
 template<typename _func>

commit e34a77f943282c67d771fb45e9c149a00ffd1672

src/Forms.cpp: Fixed vau handler dispatching.
This fixes the '#ignore' handling in the combiner creation, which should ignore the environment symbol. The incorrect handling actually would bind the dynamic environment to the symbol '', so the following code (without '' already defined) would be wrongly accepted:

() ($vau () #ignore '')

This was broken since commit edc7bc0b438a87da7e0b8b8683d4858b05b938b3.

Signed-off-by: He Bin <hebin@uniontech.com>

src/Forms.cpp
@@ -9,6 +9,7 @@
 #include "Exception.h" // for InvalidSyntax, ThrowTypeErrorForInvalidType,
 //	TypeError, ArityMismatch, ThrowListTypeErrorForNonList, UnilangException,
 //	ThrowValueCategoryError;
+#include <ystdex/optional.h> // for ystdex::optional;
 #include <exception> // for std::throw_with_nested;
 #include "Evaluation.h" // for IsIgnore, RetainN, BindParameterWellFormed,
 //	Unilang::MakeForm, CheckVariadicArity, Form, RetainList,
@@ -58,17 +59,23 @@ ThrowFormalParameterTypeError(const TermNode& term, bool has_ref)
 	ThrowTypeErrorForInvalidType(type_id<TokenValue>(), term, has_ref);
 }
 
-YB_ATTR_nodiscard YB_PURE string
-CheckEnvironmentFormal(const TermNode& term)
+YB_ATTR_nodiscard YB_PURE ystdex::optional<string>
+ExtractEnvironmentFormal(const TermNode& term)
 {
 	try
 	{
-		return ResolveTerm([&](const TermNode& nd, bool has_ref) -> string{
+		return
+			ResolveTerm([&](const TermNode& nd, ResolvedTermReferencePtr p_ref)
+			-> ystdex::optional<string>{
 			if(const auto p = TermToNamePtr(nd))
+			{
+				if(Unilang::IsMovable(p_ref))
+					return string(std::move(*p));
 				return string(*p, term.get_allocator());
+			}
 			else if(!IsIgnore(nd))
-				ThrowFormalParameterTypeError(nd, has_ref);
-			return string(term.get_allocator());
+				ThrowFormalParameterTypeError(nd, p_ref);
+			return {};
 		}, term);
 	}
 	catch(...)
@@ -498,32 +505,37 @@ MakeCombinerEvalStruct(TermNode& term, TNIter i)
 		Unilang::AsTermNode(term.get_allocator())));
 }
 
-YB_ATTR_nodiscard DynamicVauHandler
-MakeVau(TermNode& term, bool no_lift, TNIter i, ValueObject&& vo)
-{
-	auto formals(ShareMoveTerm(Unilang::Deref(++i)));
-	auto eformal(CheckEnvironmentFormal(Unilang::Deref(++i)));
-
-	return DynamicVauHandler(std::move(formals), std::move(vo),
-		MakeCombinerEvalStruct(term, ++i), no_lift, std::move(eformal));
-}
-
 template<typename _func>
 inline ReductionStatus
-ReduceCreateFunction(TermNode& term, _func f, size_t wrap)
+ReduceCreateFunction(TermNode& term, _func f)
 {
-	term.Value = Unilang::MakeForm(term, CheckFunctionCreation(f), wrap);
+	term.Value = CheckFunctionCreation(f);
 	return ReductionStatus::Clean;
+}
+
+YB_ATTR_nodiscard ReductionStatus
+ReduceVau(TermNode& term, bool no_lift, TNIter i, ValueObject&& vo)
+{
+	return ReduceCreateFunction(term, [&]() -> ContextHandler{
+		auto formals(ShareMoveTerm(Unilang::Deref(++i)));
+		auto eformal(ExtractEnvironmentFormal(Unilang::Deref(++i)));
+		auto p_es(MakeCombinerEvalStruct(term, ++i));
+
+		if(eformal)
+			return Unilang::MakeForm(term, DynamicVauHandler(
+				std::move(formals), std::move(vo), std::move(p_es), no_lift,
+				std::move(*eformal)));
+		return Unilang::MakeForm(term, VauHandler(std::move(formals),
+			std::move(vo), std::move(p_es), no_lift));
+	});
 }
 
 ReductionStatus
 VauImpl(TermNode& term, Context& ctx, bool no_lift)
 {
 	CheckVariadicArity(term, 1);
-	return ReduceCreateFunction(term, [&]{
-		return MakeVau(term, no_lift, term.begin(), MakeParentSingleNonOwning(
-			term.get_allocator(), ctx.GetRecordPtr()));
-	}, Form);
+	return ReduceVau(term, no_lift, term.begin(),
+		MakeParentSingleNonOwning(term.get_allocator(), ctx.GetRecordPtr()));
 }
 
 ReductionStatus
@@ -539,25 +551,19 @@ VauWithEnvironmentImpl(TermNode& term, Context& ctx, bool no_lift)
 	}, tm);
 	EnsureValueTags(tm.Tags);
 	return ReduceSubsequent(tm, ctx, NameTypedReducerHandler([&, i, no_lift]{
-		return ReduceCreateFunction(term, [&]{
-			return
-				ResolveTerm([&](TermNode& nd, ResolvedTermReferencePtr p_ref){
-				return MakeVau(term, no_lift, i, [&]() -> ValueObject{
-					if(IsList(nd))
-						return MakeParent(MakeEnvironmentParent(nd.begin(),
-							nd.end(), nd.get_allocator(),
-							!Unilang::IsMovable(p_ref)));
-					if(IsLeaf(nd))
-						return MakeParentSingle(term.get_allocator(),
-							ResolveEnvironment(nd.Value,
-							Unilang::IsMovable(p_ref)));
-					ThrowInvalidEnvironmentType(nd, p_ref);
-				}());
-			}, tm);
-		}, Form);
+		return ReduceVau(term, no_lift, i, ResolveTerm([](TermNode& nd,
+			ResolvedTermReferencePtr p_ref) -> ValueObject{
+			if(IsList(nd))
+				return MakeParent(MakeEnvironmentParent(
+					nd.begin(), nd.end(), nd.get_allocator(),
+					!Unilang::IsMovable(p_ref)));
+			if(IsLeaf(nd))
+				return MakeParentSingle(nd.get_allocator(),
+					ResolveEnvironment(nd.Value, Unilang::IsMovable(p_ref)));
+			ThrowInvalidEnvironmentType(nd, p_ref);
+		}, tm));
 	}, "eval-vau-parent"));
 }
-
 
 YB_NORETURN ReductionStatus
 ThrowForUnwrappingFailure(const ContextHandler& h)