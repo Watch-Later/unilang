Watch-Later/unilang

commit 82479595288e0b1f298070c634b0798fefc73fdd

src/Main.cpp: Supported -- in the command line arguments to stop parsing the options.
Signed-off-by: He Bin <hebin@uniontech.com>

src/Main.cpp
@@ -830,7 +830,7 @@ $defv! $import! (&e .&symbols) d
 }
 
 #define APP_NAME "Unilang demo"
-#define APP_VER "0.8.96"
+#define APP_VER "0.8.97"
 #define APP_PLATFORM "[C++11] + YSLib"
 constexpr auto
 	title(APP_NAME " " APP_VER " @ (" __DATE__ ", " __TIME__ ") " APP_PLATFORM);
@@ -852,6 +852,7 @@ main(int argc, char* argv[])
 		if(xargc > 1)
 		{
 			vector<string> args;
+			bool opt_trans(true);
 			bool requires_eval = {};
 			vector<string> eval_strs;
 
@@ -859,10 +860,18 @@ main(int argc, char* argv[])
 			{
 				string arg(xargv[i]);
 
-				if(arg == "-e")
+				if(opt_trans)
 				{
-					requires_eval = true;
-					continue;
+					if(YB_UNLIKELY(arg == "--"))
+					{
+						opt_trans = {};
+						continue;
+					}
+					else if(arg == "-e")
+					{
+						requires_eval = true;
+						continue;
+					}
 				}
 				if(requires_eval)
 				{

commit 39ac33fca3356b326ef35487b63b7c9974bdc190

src/Main.cpp: Fixed incorrect execution mode entered when '--' is the sole command line argument.
This was broken since commit 82479595288e0b1f298070c634b0798fefc73fdd.

Signed-off-by: He Bin <hebin@uniontech.com>

src/Main.cpp
@@ -1076,7 +1076,7 @@ PrintHelpMessage(const string& prog)
 
 
 #define APP_NAME "Unilang interpreter"
-#define APP_VER "0.12.217"
+#define APP_VER "0.12.218"
 #define APP_PLATFORM "[C++11] + YSLib"
 constexpr auto
 	title(APP_NAME " " APP_VER " @ (" __DATE__ ", " __TIME__ ") " APP_PLATFORM);
@@ -1090,6 +1090,23 @@ RunEvalStrings(Interpreter& intp, vector<string>& eval_strs, int argc,
 		JITMain();
 	for(const auto& str : eval_strs)
 		intp.RunLine(str);
+}
+
+void
+RunInteractive(int& argc, char* argv[])
+{
+	using namespace std;
+	Interpreter intp{};
+
+	cout << title << endl << "Initializing the interpreter "
+		<< (Unilang_UseJIT ? "[JIT enabled]" : "[JIT disabled]") << " ..."
+		<< endl;
+	LoadFunctions(intp, Unilang_UseJIT, argc, argv);
+	if(Unilang_UseJIT)
+		JITMain();
+	cout << "Initialization finished." << endl;
+	cout << "Type \"exit\" to exit." << endl << endl;
+	intp.Run();
 }
 
 } // unnamed namespace;
@@ -1164,24 +1181,15 @@ main(int argc, char* argv[])
 
 				RunEvalStrings(intp, eval_strs, argc, argv);
 			}
+			else
+				RunInteractive(argc, argv);
 		}
 		else if(xargc == 1)
 		{
 			using namespace std;
 
 			Unilang::Deref(YSLib::LockCommandArguments()).Reset(argc, argv);
-
-			Interpreter intp{};
-
-			cout << title << endl << "Initializing the interpreter "
-				<< (Unilang_UseJIT ? "[JIT enabled]" : "[JIT disabled]")
-				<< " ..." << endl;
-			LoadFunctions(intp, Unilang_UseJIT, argc, argv);
-			if(Unilang_UseJIT)
-				JITMain();
-			cout << "Initialization finished." << endl;
-			cout << "Type \"exit\" to exit." << endl << endl;
-			intp.Run();
+			RunInteractive(argc, argv);
 		}
 	}, yfsig, YSLib::Alert) ? EXIT_FAILURE : EXIT_SUCCESS;
 }