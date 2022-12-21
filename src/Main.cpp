Watch-Later/unilang

commit 6451112675ab55835afbc941e77a83f888312a6b

src/Main.cpp: Extracted common implementation for script mode execution.
This also enables JIT control in execution only with evaluation from the '-e' options.

Signed-off-by: He Bin <hebin@uniontech.com>

src/Main.cpp
@@ -1076,10 +1076,21 @@ PrintHelpMessage(const string& prog)
 
 
 #define APP_NAME "Unilang interpreter"
-#define APP_VER "0.12.216"
+#define APP_VER "0.12.217"
 #define APP_PLATFORM "[C++11] + YSLib"
 constexpr auto
 	title(APP_NAME " " APP_VER " @ (" __DATE__ ", " __TIME__ ") " APP_PLATFORM);
+
+void
+RunEvalStrings(Interpreter& intp, vector<string>& eval_strs, int argc,
+	char* argv[])
+{
+	LoadFunctions(intp, Unilang_UseJIT, argc, argv);
+	if(Unilang_UseJIT)
+		JITMain();
+	for(const auto& str : eval_strs)
+		intp.RunLine(str);
+}
 
 } // unnamed namespace;
 
@@ -1144,20 +1155,14 @@ main(int argc, char* argv[])
 
 				Interpreter intp{};
 
-				LoadFunctions(intp, Unilang_UseJIT, argc, argv);
-				if(Unilang_UseJIT)
-					JITMain();
-				for(const auto& str : eval_strs)
-					intp.RunLine(str);
+				RunEvalStrings(intp, eval_strs, argc, argv);
 				intp.RunScript(std::move(src));
 			}
 			else if(!eval_strs.empty())
 			{
 				Interpreter intp{};
 
-				LoadFunctions(intp, Unilang_UseJIT, argc, argv);
-				for(const auto& str : eval_strs)
-					intp.RunLine(str);
+				RunEvalStrings(intp, eval_strs, argc, argv);
 			}
 		}
 		else if(xargc == 1)

commit 2270d9f00a77e957bf0afbe0a8c8d0c5742a527b

src/Main.cpp: Fixed QApplication initialization crash.
Qt requires 'argc' always passed as a reference.
This was broken since commit 6451112675ab55835afbc941e77a83f888312a6b.

Signed-off-by: He Bin <hebin@uniontech.com>

src/Main.cpp
@@ -1084,13 +1084,14 @@ PrintHelpMessage(const string& prog)
 
 
 #define APP_NAME "Unilang interpreter"
-#define APP_VER "0.12.219"
+#define APP_VER "0.12.220"
 #define APP_PLATFORM "[C++11] + YSLib"
 constexpr auto
 	title(APP_NAME " " APP_VER " @ (" __DATE__ ", " __TIME__ ") " APP_PLATFORM);
 
+// XXX: Reference to 'argc' is required for Qt initialization.
 void
-RunEvalStrings(Interpreter& intp, vector<string>& eval_strs, int argc,
+RunEvalStrings(Interpreter& intp, vector<string>& eval_strs, int& argc,
 	char* argv[])
 {
 	LoadFunctions(intp, Unilang_UseJIT, argc, argv);