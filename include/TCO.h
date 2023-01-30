Watch-Later/unilang

commit 0bf32634ce982def92c5fe9a50297e38e6e42bf0

{include, src}/TCO: Added class TCOAction.
Signed-off-by: He Bin <hebin@uniontech.com>

include/TCO.h
@@ -3,10 +3,95 @@
 #ifndef INC_Unilang_TCO_h_
 #define INC_Unilang_TCO_h_ 1
 
-#include "Context.h"
+#include "Evaluation.h" // for shared_ptr, Environment, ContextHandler, list,
+//	TermNode, EnvironmentGuard, ReductionStatus, Context;
+#include <ystdex/scope_guard.hpp> // for ystdex::unique_guard;
+#include <tuple> // for std::tuple;
 
 namespace Unilang
 {
+
+enum RecordFrameIndex : size_t
+{
+	ActiveCombiner,
+	ActiveEnvironmentPtr
+};
+
+using FrameRecord = std::tuple<ContextHandler, shared_ptr<Environment>>;
+
+using FrameRecordList = list<FrameRecord>;
+
+class TCOAction final
+{
+private:
+	struct GuardFunction final
+	{
+		lref<TermNode> TermRef;
+
+		PDefHOp(void, (), ) const ynothrow
+			ImplExpr(TermRef.get().Clear())
+	};
+
+	mutable decltype(ystdex::unique_guard(std::declval<GuardFunction>()))
+		term_guard;
+	mutable size_t req_lift_result = 0;
+	mutable list<ContextHandler> xgds;
+
+public:
+	mutable const ContextHandler* LastFunction{};
+	mutable EnvironmentGuard EnvGuard;
+	mutable FrameRecordList RecordList;
+
+public:
+	TCOAction(Context&, TermNode&, bool);
+	TCOAction(const TCOAction&);
+	TCOAction(TCOAction&&) = default;
+
+	TCOAction&
+	operator=(TCOAction&&) = default;
+
+	ReductionStatus
+	operator()(Context&) const;
+
+	TermNode&
+	GetTermRef() const noexcept
+	{
+		return term_guard.func.func.TermRef;
+	}
+
+	void
+	AddRecord(shared_ptr<Environment>&& p_env)
+	{
+		RecordList.emplace_front(MoveFunction(), std::move(p_env));
+	}
+
+	[[nodiscard]] lref<const ContextHandler>
+	AttachFunction(ContextHandler&&);
+
+	void
+	CompressFrameList();
+
+	void
+	CompressForContext(Context&)
+	{
+		CompressFrameList();
+	}
+
+	void
+	CompressForGuard(Context&, EnvironmentGuard&&);
+
+	[[nodiscard]] ContextHandler
+	MoveFunction();
+
+	void
+	SetupLift() const;
+	void
+	SetupLift(bool lift) const
+	{
+		if(lift)
+			SetupLift();
+	}
+};
 
 } // namespace Unilang;
 

src/TCO.cpp
@@ -1,9 +1,140 @@
 ﻿// © 2021 Uniontech Software Technology Co.,Ltd.
 
-#include "TCO.h"
+#include "TCO.h" // for std::get;
+#include <ystdex/typeinfo.h> // for ystdex::type_id;
+#include <cassert> // for assert;
+#include "Exception.h" // for UnilangException;
 
 namespace Unilang
 {
+
+TCOAction::TCOAction(Context& ctx, TermNode& term, bool lift)
+	: term_guard(ystdex::unique_guard(GuardFunction{term})),
+	req_lift_result(lift ? 1 : 0), xgds(ctx.get_allocator()), EnvGuard(ctx),
+	RecordList(ctx.get_allocator())
+{
+	assert(term.Value.type() == ystdex::type_id<TokenValue>() || !term.Value);
+}
+TCOAction::TCOAction(const TCOAction& a)
+	: term_guard(std::move(a.term_guard)),
+	req_lift_result(a.req_lift_result), xgds(std::move(a.xgds)),
+	EnvGuard(std::move(a.EnvGuard))
+{}
+
+ReductionStatus
+TCOAction::operator()(Context& ctx) const
+{
+	assert(ystdex::ref_eq<>()(EnvGuard.func.ContextRef.get(), ctx));
+
+	const auto res([&]() -> ReductionStatus{
+		if(req_lift_result != 0)
+		{
+			RegularizeTerm(GetTermRef(), ctx.LastStatus);
+			for(; req_lift_result != 0; --req_lift_result)
+				LiftToReturn(GetTermRef());
+			return ReductionStatus::Retained;
+		}
+		RegularizeTerm(GetTermRef(), ctx.LastStatus);
+		return ctx.LastStatus;
+	}());
+
+	ystdex::dismiss(term_guard);
+	{
+		const auto egd(std::move(EnvGuard));
+	}
+	while(!xgds.empty())
+		xgds.pop_back();
+	while(!RecordList.empty())
+	{
+		auto& front(RecordList.front());
+
+		std::get<ActiveCombiner>(front) = {};
+		RecordList.pop_front();
+	}
+	return res;
+}
+
+[[nodiscard]] lref<const ContextHandler>
+TCOAction::AttachFunction(ContextHandler&& h)
+{
+	ystdex::erase_all(xgds, h);
+	xgds.emplace_back();
+	swap(xgds.back(), h);
+	return ystdex::as_const(xgds.back());
+}
+
+void
+TCOAction::CompressFrameList()
+{
+	auto i(RecordList.cbegin());
+
+	ystdex::retry_on_cond(ystdex::id<>(), [&]() -> bool{
+		const auto orig_size(RecordList.size());
+
+		i = RecordList.cbegin();
+		while(i != RecordList.cend())
+		{
+			auto& p_frame_env_ref(std::get<ActiveEnvironmentPtr>(
+				*ystdex::cast_mutable(RecordList, i)));
+
+			if(p_frame_env_ref.use_count() != 1 || p_frame_env_ref->IsOrphan())
+				i = RecordList.erase(i);
+			else
+				++i;
+		}
+		return RecordList.size() != orig_size;
+	});
+}
+
+void
+TCOAction::CompressForGuard(Context& ctx, EnvironmentGuard&& gd)
+{
+	if(EnvGuard.func.SavedPtr)
+	{
+		if(auto& p_saved = gd.func.SavedPtr)
+		{
+			CompressForContext(ctx);
+			AddRecord(std::move(p_saved));
+			return;
+		}
+	}
+	else
+		EnvGuard = std::move(gd);
+	AddRecord({});
+}
+
+[[nodiscard]] ContextHandler
+TCOAction::MoveFunction()
+{
+	ContextHandler res(std::allocator_arg, xgds.get_allocator());
+
+	if(LastFunction)
+	{
+		const auto i(std::find_if(xgds.rbegin(), xgds.rend(),
+			[this](const ContextHandler& h) ynothrow{
+			return &h == LastFunction;
+		}));
+
+		if(i != xgds.rend())
+		{
+			res = ContextHandler(std::allocator_arg, xgds.get_allocator(),
+				std::move(*i));
+			xgds.erase(std::next(i).base());
+		}
+		LastFunction = {};
+	}
+	return res;
+}
+
+void
+TCOAction::SetupLift() const
+{
+	++req_lift_result;
+	if(req_lift_result == 0)
+		throw UnilangException(
+			"TCO action lift request count overflow detected.");
+}
+
 
 } // namespace Unilang;
 

commit fe21d27c469a62e7aeb414793a1fb4687a17119f

{include, src}/TCO: Fixed the order of resources release in the TCO frames.
This was broken since commit 0bf32634ce982def92c5fe9a50297e38e6e42bf0.
The fix also relies on commit f03c03fa753bc7cb2aead778be86e4072294b8a6.

Signed-off-by: He Bin <hebin@uniontech.com>

include/TCO.h
@@ -149,11 +149,11 @@ private:
 
 enum RecordFrameIndex : size_t
 {
-	ActiveCombiner,
-	ActiveEnvironmentPtr
+	ActiveEnvironmentPtr,
+	ActiveCombiner
 };
 
-using FrameRecord = pair<ValueObject, shared_ptr<Environment>>;
+using FrameRecord = pair<shared_ptr<Environment>, ValueObject>;
 
 
 class FrameRecordList : public yimpl(YSLib::forward_list)<FrameRecord>
@@ -262,7 +262,7 @@ public:
 	void
 	AddOperator(ValueObject& op) const
 	{
-		record_list.emplace_front(std::move(op), nullptr);
+		record_list.emplace_front(nullptr, std::move(op));
 	}
 
 	void